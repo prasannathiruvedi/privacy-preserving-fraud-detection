002aa4dee53f387d0796cf7f45fa0438e6786fbd
Prasanna Thiruvedi <prasannathiruvedi98@gmail.com>
2026-07-10 18:30:52 +0530

    A very basic Orchestrator has been created

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -1,48 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests
-from shared.secret_sharing import secret_sharing
+import uuid
 
 app = FastAPI()
 
+#====Classes=====
+class Session: 
+    def __init__(self, txn_id):
+        self.sessionID=uuid.uuid4()
+        self.txnID=txn_id
+
 class Transaction(BaseModel):
-    txn_id: str
+    txn_id:str
 
-# Orchestrator on 8000
-# SBI ON PORT 8001
-# HDFC ON PORT 8002
-# NPCI ON PORT 8003
-
+#====API Calls=====
 @app.post("/evaluate")
-def evaluate(transaction: Transaction):
-    
-    #sbi
-    sbi = requests.get(f"http://localhost:8001/features/{transaction.txn_id}")
-    if(sbi.status_code!=200):
-        return "SBI Server Down"
-    
-    #hdfc
-    hdfc = requests.get(f"http://localhost:8002/features/{transaction.txn_id}")
-    if(hdfc.status_code!=200):
-        return "HDFC Server Down"
-    
-    #npci
-    npci = requests.get(f"http://localhost:8003/features/{transaction.txn_id}")
-    if(npci.status_code!=200):
-        return "NPCI Server Down"
-    
-    transaction_details=sbi.json()|hdfc.json()|npci.json()
-
-    #====Under Deliberation=====
-    #shares=share_creation(transaction_details)
-    #placeholder value 1
-    return 1
-
-
-#def share_creation(transaction_details):
-    details_share=dict()
-    for key, value in transaction_details.items():
-        if isinstance(value, (int,float)):
-            details_share[key]=secret_sharing(value)
-    
-    return details_share
+def evaluate(txn_details:Transaction):
+    evalSession=Session(txn_details.txn_id)
+    return evalSession/
